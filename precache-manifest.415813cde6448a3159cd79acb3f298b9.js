8c1236cf25e8da713fabb30b4ec000833d9a46f6
Aditya Vashisht <vash0005@algonquinlive.com>
2020-07-26 17:29:15 -0400

    Updates

---
+++ precache-manifest.415813cde6448a3159cd79acb3f298b9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5077cb76893e825a8129a5eef3f9660a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e529e1c01148e47715cb",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e7539bfa5835ba0903bb",
+    "url": "/portfolio/static/css/main.73dc2927.chunk.css"
+  },
+  {
+    "revision": "e529e1c01148e47715cb",
+    "url": "/portfolio/static/js/2.f073f0f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.f073f0f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7539bfa5835ba0903bb",
+    "url": "/portfolio/static/js/main.f579bf35.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "6c394532c06f2b5599aacce7246d37eb",
+    "url": "/portfolio/static/media/Hola-app.6c394532.png"
+  },
+  {
+    "revision": "de73966a782e459e61012bb12c6a1624",
+    "url": "/portfolio/static/media/Hola-dashboard.de73966a.png"
+  },
+  {
+    "revision": "a9aab92ef66d065132e89ecaecbfa5dc",
+    "url": "/portfolio/static/media/hello.a9aab92e.png"
+  },
+  {
+    "revision": "23a7b61b7ddec66d53ea5277b2bd9e01",
+    "url": "/portfolio/static/media/logo.23a7b61b.jpeg"
+  }
+]);
